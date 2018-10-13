2fea91f2f2f0c8323423d4991e95300597d567ba
Alex Sato <as4628@columbia.edu>
2018-10-13 04:18:21 -0400

    game over after certain number of tries implemented

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,23 +1,26 @@
-
-var wordBank = ["Iron maiden","wintersun"]
+//
+//Global Variables used for game
+//
+var wordBank = ["IRON MAIDEN","WINTERSUN"]
 var guesses_left = 6;
+var badGuess = [];
+var score = 0;
 
-// function gameSetup(word, display){
-//     var blank = "";
-//     for(var i=0; i<word.length; i++){
-//         if(word[i]==" ")
-//             blank += "&nbsp &nbsp";
-//         else
-//             blank += "_ ";
-//     }
-//     var blank_display = document.createElement("div");
-//     blank_display.innerHTML = blank;
-//     display.appendChild(blank_display); 
-    
-// };
+
+//Initializes the game.
+//1) Sets up the initial display of the game.
+//  The display should show '_' for each letter.
+//  A space is inserted in between each letter for clarity.
+//2) Sets each '_' with an attribute for the letter it represents.
+//  This is done to keep track of what each letter is.
+//3) Clears the badGuess array.
+//
+// **You can easily cheat in this game by inspecting...** 
 
 function gameSetup(word){
-    
+    var playerDisplayDiv = document.getElementById("player_status");
+    playerDisplayDiv.textContent="Guesses left: " + guesses_left;
+
     for(var i=0; i<word.length; i++){
         var letter = $("<div>");
         if (word.charAt(i)==" "){
@@ -31,53 +34,52 @@ function gameSetup(word){
         }
         $("#game_board").append(letter);    
     }
-    
+    badGuess = []; 
 };
 
 
-function is_game_running(guesses_left, word){
-    alert("test");
-    if (guesses_left == 0)
-        return false;
-    else{
-        for (var i = 0; i<word.length; i++ ) {
-            if (word[i]="_")
-                return true;
-            else
-                continue;
-        }
-    }
-
-    return false;
-};
-
-var targetDiv = document.getElementById("band_name");
-    targetDiv.textContent = "Computer's choice: ";
-    for(var i = 0; i<wordBank[0].length; i++){
+// var targetDiv = document.getElementById("band_name");
+//     targetDiv.textContent = "Computer's choice: ";
+//     for(var i = 0; i<wordBank[0].length; i++){
         
-        var newLetter = document.createElement("div")
-        newLetter.textContent = wordBank[0].charAt(i);
-        targetDiv.appendChild(newLetter);
-    }
+//         var newLetter = document.createElement("div")
+//         newLetter.textContent = wordBank[0].charAt(i);
+//         targetDiv.appendChild(newLetter);
+//     }
 
-var playerDisplayDiv = document.getElementById("player_status");
-playerDisplayDiv.textContent="Player display: ";
-// gameSetup(wordBank[0],playerDisplayDiv);
 gameSetup(wordBank[0]);
 
-// $(document).keypress(function(event) {
-//     alert('Handler for .keypress() called. - ' + event.charCode);
-// });
 
-
+//Takes input from player.
+//If letter is found, it's displayed.
+//If letter is not found, nothing yet.
 $(document).on ("keypress",function(){
-    var guess = event.key;
-    $(".letter").each(function(){
-        if(   (event.key == $(this).attr("data"))   || (event.key) == $(this).attr("data"))
-            $(this).text(event.key); 
+    
+    if (guesses_left > 0){
+        var guess = (event.key).toUpperCase();
+        var loseLife = true;
+        $(".letter").each(function(){
+            if(guess == $(this).attr("data")){
+                $(this).text(guess);
+                loseLife = false;
+            }
+        });
+        for(var i=0; i<badGuess.length; i++){
+            if(guess == badGuess[i])
+                loseLife=false;
+        }
+        badGuess.push(guess); //I realize that this is a bit problematic.
+                            //Repeat bad guesses will be added onto this array.
+                            //It's not great for efficiency as a result.
+                            //But fortunately that's not a grading criteria!
+        if (loseLife){
+            guesses_left--;
+            $("#player_status").text("Guesses left: " + guesses_left);
+        }
 
-    });
-
+    }
+    else
+        alert("Game over!");
 });
 
 
